9cf016a00f09472759e2fe99f072d3bcf649ee07
Okko Hartikainen <okko.hartikainen@gmail.com>
2020-12-22 20:12:20 +0200

    uibuild

--- index.js
+++ index.js
@@ -122,7 +122,8 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    return response.status(400).send({ error: 'validation error' })
+//    return response.status(400).json({ error: error.message })
   }
 
   next(error)
